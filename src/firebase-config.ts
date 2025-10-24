bde925a222de917331cbedb97d02f86e93878b9c
GerardAsuncion <asunciongerard90@gmail.com>
2025-10-24 15:01:27 +0200

    feat: getAuth and GoogleAuthProvider added to firebase-config

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, type FirebaseApp } from "firebase/app";
 import { Firestore, getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -29,3 +30,5 @@ const firebaseConfig: firebaseConfigType = {
 // Initialize Firebase
 const app: FirebaseApp = initializeApp(firebaseConfig);
 const db: Firestore = getFirestore(app);
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
