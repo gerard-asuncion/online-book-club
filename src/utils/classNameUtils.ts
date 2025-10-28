e777dbb26d8a9bbcfd556c3ac7428d001891fc68
GerardAsuncion <asunciongerard90@gmail.com>
2025-10-28 11:13:31 +0100

    fix: deleted unused functions from classNameUtils

--- src/utils/classNameUtils.ts
+++ src/utils/classNameUtils.ts
@@ -1,12 +1,3 @@
-export const showHide = (show: boolean): string =>
-    show ? "" : "hidden";
-
-export const narrowChat = (narrow: boolean): string =>
-    narrow ? "md:col-span-2" : "md:col-span-3";
-
-export const makeFullHeight = (fullHeight: boolean): string =>
-    fullHeight ? "h-full" : "hidden";
-
 export const changeWindowLayout = (openSidebar: boolean): string =>
     openSidebar ?
     "hidden md:block md:col-span-2" :
